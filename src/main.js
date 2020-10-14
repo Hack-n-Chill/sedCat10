bb30b55a240cc30cc8c9181b1ccdf4379d541d71
SanpuiRonak <ronaksanpui@gmail.com>
2020-10-15 00:08:36 +0530

    Added dynamic commands
    Signed-off-by: SanpuiRonak <ronaksanpui@gmail.com>

--- src/main.js
+++ src/main.js
@@ -1,17 +1,41 @@
 const Discord = require("discord.js");
-const client = new Discord.Client();
 require("dotenv").config();
+const fs = require("fs");
 const logActivity = require("debug")("Log Activity");
 
 const token = process.env.token;
+const client = new Discord.Client();
 
 client.once("ready", () => {
   logActivity("Bot Now Online!");
 });
 
-client.on("message", (msg) => {
-  if (msg.content === ";ping") {
-    msg.channel.send("Pong Pong.");
+//New Colection for dyanimacally storing commands
+client.commands = new Discord.Collection();
+const prefix = ";";
+
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
+}
+
+client.on("message", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  if (!client.commands.has(command)) return;
+
+  try {
+    client.commands.get(command).execute(message, args);
+  } catch (error) {
+    console.error(error);
+    message.reply("there was an error trying to execute that command!");
   }
 });
 
